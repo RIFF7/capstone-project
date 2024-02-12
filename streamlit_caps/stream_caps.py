c6d09e3a3ccda753f914bacf0bf5e3dad4c05234
RIFF7 <syarifudinjaelani@gmail.com>
2024-02-12 22:18:28 +0700

    Remove code pada stream_caps.py karena kesalahan import data

--- streamlit_caps/stream_caps.py
+++ streamlit_caps/stream_caps.py
@@ -5,6 +5,11 @@ import numpy as np
 import altair as alt
 from PIL import Image
 
+st.set_page_config(
+    page_title = 'Belajar Streamlit',
+    layout='wide'
+)
+
 # Mengatur tata letak judul ke tengah dengan CSS
 st.markdown(
     f"""
@@ -31,8 +36,8 @@ st.title('Analisa Kasus Kejahatan Pada Negara US (Los Angeles)')
 stringHeader = 'Penulis : **Syarifudin Jaelani**'
 st.markdown(stringHeader)
 
-image = Image.open('dataset_caps/crime1.jpg')
-st.image(image, caption='')
+image = Image.open('dataset_caps/crime2.jpg')
+st.image(image, caption='', use_column_width=True)
 
 # ---------------------------------------------------------------
 
@@ -44,27 +49,38 @@ stringInfo1 = '''
             
             1. [Data.gov - Crime Data from 2020 to Present](https://catalog.data.gov/dataset/crime-data-from-2020-to-present)
             
-            2. [LA City - Crime Data](https://data.lacity.org/browse)
+            2. [LA City - Crime Data](https://data.lacity.org/Public-Safety/Crime-Data-from-2020-to-Present/2nrs-mtv8/about_data)
             '''
 st.sidebar.info(stringInfo1)
 
 stringInfo2 = '''
             **Penjelasan Mengenai Dataset Tindak Kejahatan**
             
-            Prosess....
+            Kumpulan data ini mencerminkan insiden kejahatan di Kota Los Angeles sejak tahun 2020 hingga saat ini. 
+            Data ini ditranskrip dari laporan kejahatan asli yang diketik di atas kertas dan oleh karena itu 
+            mungkin ada beberapa ketidakakuratan dalam data. Beberapa bidang lokasi dengan data yang hilang 
+            dicatat sebagai (0°, 0°). Kolom alamat hanya disediakan hingga seratus blok terdekat untuk menjaga privasi. 
+            Data ini seakurat data yang ada di database.
             '''
 st.sidebar.info(stringInfo2)
 
 stringInfoAuthor = '''
-            **Syarifudin Jaelani**
+            **Kontak Saya:**
             
-            Prosesss....           
+            📬 syarifudinjaelani@gmail.com\n
+            📲 [LinkedIn](https://www.linkedin.com/in/syarifudin-jaelani-b956761b3/)\n
+            📰 [Medium](https://medium.com/@syarifudinjaelani)\n
+            📑 [GitHub](https://github.com/RIFF7)\n
+            🧾 [Instagram](https://www.instagram.com/paper.curt/)
             '''
 st.sidebar.info(stringInfoAuthor)
 
 # Penjelasan Singkat Mengenai Tidak Kejahatan
 string1 = '''
-         Prosess....
+         Dalam era digital yang terus berkembang, analisis data telah menjadi salah satu alat yang paling kuat dalam memahami dan mengatasi tantangan keamanan masyarakat. Dengan adanya akses terhadap dataset kriminalitas yang kaya informasi, kita dapat mengeksplorasi pola-pola yang tersembunyi dan mendapatkan wawasan yang berharga untuk meningkatkan efektivitas penegakan hukum serta keamanan masyarakat.\n
+         Artikel ini mengusung tujuan untuk memberikan pandangan mendalam terhadap tren dan pola kejahatan yang terjadi sejak tahun 2020 hingga saat ini. Dataset yang digunakan merupakan kumpulan data laporan kejahatan yang mencakup beragam informasi, mulai dari tanggal kejadian, lokasi, hingga deskripsi kejahatan. Analisis yang dilakukan tidak hanya sekedar mencari statistik umum, tetapi juga mencoba untuk melihat lebih jauh, mengidentifikasi pola yang mungkin tidak terlihat pada pandangan pertama.\n
+         Melalui artikel ini, penulis akan memperkenalkan pembaca pada hasil analisis mendalam terhadap beberapa variabel kunci seperti nomor laporan, tanggal pelaporan, lokasi kejadian, jenis kejahatan, serta karakteristik korban dan status laporan. Semua informasi yang disajikan berdasarkan data aktual yang telah dipersiapkan dan diolah sebelumnya, memberikan keandalan dan keakuratan yang diperlukan dalam mengambil kesimpulan.\n
+         Diharapkan artikel ini tidak hanya memberikan wawasan baru tentang kejahatan yang terjadi di komunitas kita, tetapi juga menjadi landasan untuk kebijakan dan langkah-langkah yang lebih efektif dalam mencegah dan menanggulangi kejahatan. Mari kita bersama-sama menjelajahi dunia kejahatan melalui lensa data, untuk menciptakan lingkungan yang lebih aman bagi kita semua.
          '''
 st.write(string1)
 
@@ -75,52 +91,5 @@ st.title('Analisa Tren Waktu Kejahatan')
 
 # Import Dataset
 data_year = pd.read_csv('dataset_caps/yearly_report_count.csv')
-
-# Buat daftar tahun unik
-tahun_unik = list(data_year['Year_Occ'].unique())
-
-# Buat pilihan dropdown untuk tahun pertama
-tahun_pertama = st.selectbox('Pilih Tahun Pertama:', tahun_unik)
-
-# Buat pilihan dropdown untuk tahun kedua
-tahun_kedua = st.selectbox('Pilih Tahun Kedua:', tahun_unik)
-
-# Filter data berdasarkan tahun yang dipilih
-df_filtered1 = data_year[data_year['Year_Occ'] == tahun_pertama]
-df_filtered2 = data_year[data_year['Year_Occ'] == tahun_kedua]
-
-# Buat plot dengan Altair untuk perbandingan antara tahun pertama dan tahun kedua
-chart = alt.Chart(df_filtered1).mark_line().encode(
-    x=alt.X('Month_Occ', title='Bulan'),
-    y=alt.Y('Jml_Kejahatan', title='Jumlah Kejahatan'),
-    color=alt.value('blue'),
-    tooltip=['Year_Occ:N', 'Jml_Kejahatan:Q']  # Menambahkan tooltip untuk tahun dan jumlah kejahatan
-).properties(
-    width=600,
-    height=400
-)
-
-chart += alt.Chart(df_filtered2).mark_line().encode(
-    x=alt.X('Month_Occ', title='Bulan'),
-    y=alt.Y('Jml_Kejahatan', title='Jumlah Kejahatan'),
-    color=alt.value('red'),
-    tooltip=['Year_Occ:N', 'Jml_Kejahatan:Q']  # Menambahkan tooltip untuk tahun dan jumlah kejahatan
-)
-
-# Tambahkan titik pada setiap lekukan grafik
-chart += alt.Chart(df_filtered1).mark_circle(color='blue').encode(
-    x=alt.X('Month_Occ', title='Bulan'),
-    y=alt.Y('Jml_Kejahatan', title='Jumlah Kejahatan'),
-    tooltip=['Year_Occ:N', 'Jml_Kejahatan:Q']  # Menambahkan tooltip untuk tahun
-)
-
-chart += alt.Chart(df_filtered2).mark_circle(color='red').encode(
-    x=alt.X('Month_Occ', title='Bulan'),
-    y=alt.Y('Jml_Kejahatan', title='Jumlah Kejahatan'),
-    tooltip=['Year_Occ:N', 'Jml_Kejahatan:Q']  # Menambahkan tooltip untuk tahun
-)
-
-# Tampilkan plot menggunakan Streamlit
-st.altair_chart(chart, use_container_width=True)
 
 # ---------------------------------------------------------------
